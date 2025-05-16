aca1a9156a485c72ff2fc02d6181c98ef9899347
marlenesv <marlenesaldana13@gmail.com>
2025-05-16 09:10:23 -0600

    refactorizacion

--- html00/Formulario.js
+++ html00/Formulario.js
@@ -1,15 +1,32 @@
 console.log("inicio")
 
-const boton = document.getElementById("enviar")
+// const boton = document.getElementById("enviar")
 
-boton.addEventListener("click",
-    () => {
-        console.log("los parametros")
-        let user=document.getElementById("user")
-        let password=document.getElementById("password")
-        console.log(password.value)
-        CloseEvent.log(user.value)
+// boton.addEventListener("click",
+//     () => {
+    //     console.log("los parametros")
+    //     let user=document.getElementById("user")
+    //     let password=document.getElementById("password")
+    //     console.log(password.value)
+    //     console.log(user.value)
         
 
+    // }
+
+    const mostrar = () => {
+           console.log("los parametros")
+           let user=document.getElementById("user")
+           let password=document.getElementById("password")
+           console.log(password.value)
+           console.log(user.value)
+
+           let formulario = document.getElementsByTagName("form")
+           console.log(formulario)
+           user.setAttribute("name", "q")
+           password.setAttribute("name", "password")
+           formulario[0].setAttribute("action","https://google.com/search")
+           formulario[0].submit()
     }
-)
+
+    const boton = document.getElementById("enviar")
+    boton.addEventListener("click", mostrar)
